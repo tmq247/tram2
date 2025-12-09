03862d3cf31b8a49a1ff63fa0289cc77d7858a6f
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-12-10 05:35:35 +0700

    Update userbot.py

--- AnnieXMedia/core/userbot.py
+++ AnnieXMedia/core/userbot.py
@@ -9,11 +9,7 @@ assistants = []
 assistantids = []
 
 GROUPS_TO_JOIN = [
-    "CertifiedDiscussion",
-    "CertifiedCoders",
-    "CertifiedCodes",
-    "CertifiedDevs",
-    "CertifiedNetwork",
+    "muoimuoimusicbot",
 ]
 
 
